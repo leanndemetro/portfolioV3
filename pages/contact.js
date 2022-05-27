5735afd2123da42ccf98741cfa1061a381628ccd
Alyssa DeMetro <alyssademetro@Alyssas-MacBook.lan>
2022-05-27 17:03:16 -0400

    yes

--- pages/contact.js
+++ pages/contact.js
@@ -32,7 +32,7 @@ export default function Example() {
         };
 
         const response = await axios(config);
-        console.log(response);
+        router.push("/");
       
 
 
@@ -71,7 +71,7 @@ export default function Example() {
                     </div>
                     <div className="bg-black py-16 px-4 sm:px-6 lg:col-span-3 lg:py-24 lg:px-8 xl:pl-12">
                         <div className="max-w-lg mx-auto  lg:max-w-none">
-                            <form action="#" method="POST" className="grid grid-cols-1 gap-y-6">
+                            <form className="grid grid-cols-1 gap-y-6">
                                 <div>
                                     <label htmlFor="full-name" className="sr-only">
                                         Full name
