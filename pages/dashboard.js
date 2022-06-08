fc7873d4323dbabb07e7e5495ff6d040fc5e4c49
Alyssa DeMetro <alyssademetro@Alyssas-MacBook.lan>
2022-06-08 15:28:53 -0400

    fix

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import Nav from "../components/nav";
 import Footer from "../components/footer";
-import Router from "next/router";
+
 
 
 
@@ -47,6 +47,7 @@ export default function Dashboard() {
       };
 
     const handleClick = event => {
+    console.log(event.target.id)
     setId(event.target.id);
     handleSubmit();
   };
@@ -104,7 +105,7 @@ export default function Dashboard() {
                                                 type="button"
                                                 onClick={handleClick}
                                                 className=" hover:text-indigo-900">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                                                    <svg id={post.id} xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                                                         <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
                                                     </svg>
                                                     
